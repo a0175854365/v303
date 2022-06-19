63bd4e1802216c321f95d38031a91ab34a309d94
Hyunsung Lee <a0175854365@gmail.com>
2022-06-20 00:11:28 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginVue />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import LoginVue from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    LoginVue
   }
 }
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container box bg_lightgray mb-3">
+        <h1>Login vue component</h1>
+        <h2>
+            Bootstrap 적용, Custom CSS 적용
+        </h2>
+        <div class="row mb-3">
+            <div class="col-6">
+                <img alt="Vue logo" src="../assets/logo.png">
+            </div>
+            <div class="col-6">
+                <h2>Login</h2>
+                <div class="mb-3">
+                    <input class="form-control" type="text" placeholder="Default input"
+                        aria-label="default input example">
+                    <input class="form-control" type="text" placeholder="Default input"
+                        aria-label="default input example">
+                    <button type="button" class="btn btn-primary">Login</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginVue',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
